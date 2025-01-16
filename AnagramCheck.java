bb9671e2f4a2db9327e319ba296a6314dd531138
Claudius26 <osomzed@gmail.com>
2025-01-16 06:37:42 -0800

    Add this

---
+++ AnagramCheck.java
@@ -0,0 +1,20 @@
+public class AnagramCheck{
+
+	public static void main(String... args){
+
+		String word = "anagram";
+
+		for(int count = 0; count < word.length();count++){
+
+		System.out.print(word.charAt(count) + " ");
+
+
+		}
+
+		
+
+	}
+
+
+
+}
